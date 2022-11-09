73c3604b59b96b33d75536f85c675dc5d12c21ad
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2022-11-09 01:25:52 +0100

    Beginning of shell.c

---
+++ shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Simple Shell (shell)
+ * @argc: Arguement count
+ * @argv: Arguement value
+ * Return: 
+ */
+
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	char *input, **cmd;
+	int counter = 0, statue = 1, st = 0;
+
+	if (argv[1[
