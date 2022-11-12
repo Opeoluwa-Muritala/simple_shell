a7797ad78c216effd0e5eec08e39075c29983247
Opeoluwa-Muritala <muritalaopeoluwa102gmail.com>
2022-11-12 13:46:45 +0100

    Completed shell.c

--- shell.c
+++ shell.c
@@ -12,4 +12,58 @@ int main(__attribute__((unused)) int argc, char **argv)
 	char *input, **cmd;
 	int counter = 0, statue = 1, st = 0;
 
-	if (argv[1[
+	if (argv[1] != NULL)
+	{
+		read_file(argv[1], argv);
+	}
+
+	signal(SIGINT, signal_to_handel);
+
+	while (statue)
+	{
+		counter++;
+		if (isatty(STDIN_FILENO))
+		{
+			prompt();
+		}
+		input = _getline();
+		if (input[0] == '\0')
+		{
+			continue;
+
+		}
+		history(input);
+		cmd = parse_cmd(input);
+		if (_strcmp(cmd[0], "exit") == 0)
+		{
+			exit_bul(cmd, input, argv, counter);
+		}
+		else if (check_builtin(cmd) == 0)
+		{
+			st = handle_builtin(cmd,st);
+			continue;
+		}
+		else
+		{
+			st = check_cmd(cmd, input, counter, argv);
+		}
+		free_all(cmd, input);
+	}
+	return (statue);
+}
+
+/**
+ * create_enviroment - Create array of enviromental variable
+ * @envi: Array of Enviromental Variable
+ * Return: Void
+ */
+void create_enviroment(char **envi)
+{
+	int i;
+
+	for (i = 0; environ[i]; i++)
+	{
+		envi[i] = _strdup(environ[i]);
+	}
+	envi[i] = NULL;
+}
